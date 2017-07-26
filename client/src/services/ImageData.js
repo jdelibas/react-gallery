9bfecc7bd8bb3e9b942f4fb0d24ca3adf76dda54
James Delibas <jdelibas@gmail.com>
2017-07-26 09:09:42 +0100

    pass url and fix trailing slash

--- client/src/services/ImageData.js
+++ client/src/services/ImageData.js
@@ -5,11 +5,10 @@ import axios from 'axios'
 export default class ImageData {
   constructor () {
     // webpack provided global
-    console.log(process.env.IMAGE_API)
-    // this.url = process.env.IMAGE_API
+    this.url = process.env.IMAGE_API
   }
 
   getPhotosByTag (tag) {
-    return axios.get(`${this.url}/&tags=${tag}`)
+    return axios.get(`${this.url}?tags=${tag}`)
   }
 }
